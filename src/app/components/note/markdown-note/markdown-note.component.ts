b46a707768822e200d52c94b640765f494ca821b
Digimezzo <digimezzo@outlook.com>
2026-02-28 23:41:23 +0100

    Fix #210: Pressing 'Tab' in a note makes the cursor disappear

--- src/app/components/note/markdown-note/markdown-note.component.ts
+++ src/app/components/note/markdown-note/markdown-note.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Input, ViewChild, ViewEncapsulation} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { INoteEditor } from '../note-editor/i-note-editor';
 
 @Component({
@@ -10,7 +10,7 @@ import { INoteEditor } from '../note-editor/i-note-editor';
 export class MarkdownNoteComponent implements AfterViewInit {
     @ViewChild('highlightedContent') highlightedContent: ElementRef<HTMLPreElement>;
     @ViewChild('markdownInput') markdownInput: ElementRef<HTMLTextAreaElement>;
-    
+
     @Input()
     public noteEditor: INoteEditor;
 
@@ -33,24 +33,30 @@ export class MarkdownNoteComponent implements AfterViewInit {
         pre.scrollLeft = input.scrollLeft;
     }
 
+    public onTabKey(event: Event): void {
+        event.preventDefault();
+        const textarea = this.markdownInput.nativeElement;
+
+        // Use execCommand to insert spaces so the action is undoable (Ctrl+Z)
+        document.execCommand('insertText', false, '    ');
+    }
+
     public toggleIsEditing(): void {
         this.noteEditor.isEditing = !this.noteEditor.isEditing;
     }
 
     public highlightMarkdown(text: string): string {
-        if(!text) {
+        if (!text) {
             return '';
         }
 
         // Handle final newlines (See: https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/)
-        if(text[text.length-1] == "\n") { // If the last character is a newline character
-            text += " "; // Add a placeholder space character to the final line 
+        if (text[text.length - 1] == '\n') {
+            // If the last character is a newline character
+            text += ' '; // Add a placeholder space character to the final line
         }
-        
-        const escaped = text
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
+
+        const escaped = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
         const highlighted = escaped
             .replace(/^### (.*)$/gm, '<span class="md-title-3">### $1</span>')
